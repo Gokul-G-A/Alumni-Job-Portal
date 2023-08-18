84e96e4e2b1896aaa52f0f27325f25dce469595d
Gokul-G-A <142618110+Gokul-G-A@users.noreply.github.com>
2023-08-18 23:18:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const morgan=require('morgan')
 const app=express();
 var cors = require('cors');
 app.use(cors());
+require('dotenv').config();
+require('./db/mongodb');
+app.use(morgan("dev"));
+
+const path = require('path'); 
+app.use(express.static(path.join(__dirname,'/build')));
+
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type ");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+})
+
 
 const jobRoutes = require('./routes/jobRoutes');
 const userRoute=require('./routes/userRoute');
@@ -16,9 +32,13 @@ app.use('/api', jobRoutes);
 app.use('/api/apply', applyRoutes);
 app.use('/api', userProfileRoute);
 
-require('dotenv').config();
-require('./db/mongodb');
-app.use(morgan("dev"));
+
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname,'/build/index.html'));
+ });
+
+
 const PORT=5000;
 app.listen(PORT,()=>{
     console.log(`Server running on PORT ${PORT}`);
